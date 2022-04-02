497370bf990123896249d6816c2b4ff72a4b8a0d
Tomek Marciniak <tmarciniakm@gmail.com>
2022-04-02 13:28:23 +0200

    feat(prose): allow passing all allowed props to Prose component

--- packages/chakra-ui-prose/src/prose.tsx
+++ packages/chakra-ui-prose/src/prose.tsx
@@ -1,15 +1,11 @@
-import * as React from 'react';
-import { chakra, HTMLChakraProps, useStyleConfig } from '@chakra-ui/system';
+import * as React from "react";
+import { chakra, HTMLChakraProps, useStyleConfig } from "@chakra-ui/system";
 
-export interface ProseProps extends HTMLChakraProps<'div'> {}
+export interface ProseProps extends HTMLChakraProps<"div"> {}
 
 export function Prose(props: ProseProps) {
-  const { children, as = 'article' } = props;
-  const styles = useStyleConfig('Prose', { ...props });
+  const { as = "article", ...rest } = props;
+  const styles = useStyleConfig("Prose", { ...props });
 
-  return (
-    <chakra.div as={as} __css={{ ...styles }}>
-      {children}
-    </chakra.div>
-  );
+  return <chakra.div as={as} __css={{ ...styles }} {...rest} />;
 }
